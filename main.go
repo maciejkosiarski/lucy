ae0e5f519bde09d508cc4c4f85095170afcb56aa
Marcin Malessa <marcin.malessa@performance-media.pl>
2022-09-17 20:54:54 +0200

    Add flag: 'cluster'

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"flag"
+	"strings"
+
 	"github.com/maciejkosiarski/lucy/forwarder"
 )
 
 func main() {
-	forwarder.ForwardPorts()
+
+	var arg_c string
+	flag.StringVar(&arg_c, "c", "", "Cluster names (coma separated). Default all.")
+	flag.Parse()
+	cluster_names := ParseClusterNames(arg_c)
+
+	f := forwarder.Forwarder{
+		ConfigFileName: "forwarder.yaml",
+		ClusterNames:   cluster_names,
+	}
+	f.ForwardPorts(cluster_names)
+}
+
+func ParseClusterNames(value string) []string {
+	s := strings.Split(value, ",")
+	for i := range s {
+		s[i] = strings.TrimSpace(s[i])
+	}
+	return s
 }
